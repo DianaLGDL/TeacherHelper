708139a12e0f800857a4e2f416c41cb4bec4a800
Diana García <Diana Garca@DESKTOP-K3HAI1R>
2016-06-14 08:41:02 -0500

    Se creo el script inicial de la Base de Datos

---
+++ TeacherHelper/dbTeacherHelperESC.sql
@@ -0,0 +1,6 @@
+﻿/*Nombre script: dbEasyPayESC
+Descripcion: Creación de la scribt de la base de datos se creo el script inicial
+Version: 1.0
+Historia de la modificación:  IS               Fecha
+                             DLGDL            14/06/16
+							 */

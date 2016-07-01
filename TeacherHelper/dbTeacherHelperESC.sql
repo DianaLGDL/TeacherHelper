5f0de6991acee0dd78c8914d5d212272d3420986
Diana García <Diana Garca@DESKTOP-K3HAI1R>
2016-07-01 08:17:18 -0500

    Nuevos cambios Cinthia

--- TeacherHelper/dbTeacherHelperESC.sql
+++ TeacherHelper/dbTeacherHelperESC.sql
@@ -3,4 +3,6 @@ Descripcion: Creación de la scribt de la base de datos se creo el script inicia
 Version: 1.0
 Historia de la modificación:  IS               Fecha
                              DLGDL            14/06/16
-							 DLGDL            01/07/16*/
+							 DLGDL            01/07/16
+							 CMHB             01/07/16*/
+ 

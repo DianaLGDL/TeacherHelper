8a14b29a440381b6cc75bdb47a117ebcb604fd20
michelxix <michel.hdz02@hotmail.com>
2016-07-02 13:50:59 -0500

    pts

--- TeacherHelper/TeacherHelperWinForm/FormLogin.cs
+++ TeacherHelper/TeacherHelperWinForm/FormLogin.cs
@@ -10,11 +10,21 @@ using System.Windows.Forms;
 
 namespace TeacherHelperWinForm
 {
-    public partial class Form1 : Form
+    public partial class FormLogin : Form
     {
-        public Form1()
+        public FormLogin()
         {
             InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }

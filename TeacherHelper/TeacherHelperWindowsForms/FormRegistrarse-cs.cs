66daf92f6ef6c573eca2a833c7ea595ad5ddc9b1
michelxix <michel.hdz02@hotmail.com>
2016-07-02 22:49:16 -0500

    2

---
+++ TeacherHelper/TeacherHelperWindowsForms/FormRegistrarse-cs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TeacherHelperWindowsForms
+{
+    public partial class FormRegistrarse_cs : Form
+    {
+        public FormRegistrarse_cs()
+        {
+            InitializeComponent();
+        }
+    }
+}

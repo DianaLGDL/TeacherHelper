1827a823233863d64bbfacde0c0e98b6b3cf058c
michelxix <michel.hdz02@hotmail.com>
2016-07-03 00:08:48 -0500

    3

--- TeacherHelper/TeacherHelperWindowsForms/FormRegistrarse-cs.Designer.cs
+++ TeacherHelper/TeacherHelperWindowsForms/FormRegistrarse-cs.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormRegistrarse_cs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 331);
+            this.Name = "FormRegistrarse_cs";
             this.Text = "FormRegistrarse_cs";
+            this.ResumeLayout(false);
+
         }
 
         #endregion

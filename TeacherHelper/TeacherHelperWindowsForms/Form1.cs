88366733949b217a637a13b1ee507b6b5a42dd83
Brandon Lobo <negalobosangriento@outlook.es>
2016-08-01 19:15:25 -0500

    se creo Login y sus configuraciones restantes

--- TeacherHelper/TeacherHelperWindowsForms/Form1.cs
+++ TeacherHelper/TeacherHelperWindowsForms/Form1.cs
@@ -1,4 +1,4 @@
-﻿#region librerias
+﻿#region librerias ("BNL")
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -18,27 +18,33 @@ namespace TeacherHelperWindowsForms
         {
             InitializeComponent();
         }
+
+        #region form ("BNL")
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-        #region botones
-        private void btnEntrar_Click(object sender, EventArgs e)
-        {
-            if (UsuarioDAL.Autentificar(txtUsuario.Text, txtContraseña.Text) > 0)
-            {
-                TeacherHelper t = new TeacherHelper();
-                t.ShowDialog();
-            }
-            else
-                MessageBox.Show("Error en los datos");
-        } 
+        #endregion
 
-        private void btnRegistrarse_Click(object sender, EventArgs e)
+        #region botones ("BNL")
+
+        private void btnRegistrarse_Click_1(object sender, EventArgs e)
         {
             FormRegistrarse_cs r = new FormRegistrarse_cs();
             r.ShowDialog();
         }
+
+        private void btnEntrar_Click_1(object sender, EventArgs e)
+        {
+            if (UsuarioDAL.Autentificar(txtUsuario.Text, txtContraseña.Text) > 0)
+            {
+                FormGrupos g = new FormGrupos();
+                g.ShowDialog();
+            }
+            else
+                MessageBox.Show("Error en los datos");
+        }
     }
-}
+    }
+
 #endregion
